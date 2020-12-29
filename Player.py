c1b1d9d77033791bd522b3f561da046bfb0a2140
aaroncp <aaroncplunkett@gmail.com>
2020-12-29 05:04:54 -0600

    allocateStats is now generating scores on a per hero basis, but doing nothing with it yet

--- Player.py
+++ Player.py
@@ -17,46 +17,51 @@ class Player:
   def allocateStats(self):
       statName = ['Eliminations','Damage Done','Healing Done','Deaths']
       elims,damage,healing,deaths = None,None,None,None
+      perMapPerHeroDFs = []
       elimsList, damageList, healingList, deathsList = [],[],[],[]
 
       #new dataframe of rows with unique date and map name
       uniqueDateMapDF = self.allPlayerData.drop_duplicates(subset=['start_time','map_name'])
+      uniqueHeroesList = uniqueDateMapDF.hero_name.unique()
+      uniqueHeroesList = uniqueHeroesList.remove('All Heroes')
       uniqueDateMapList = uniqueDateMapDF.values.tolist()
       self.numOfMaps = len(uniqueDateMapDF.index)
       
       #For each map
       for i in range(len(uniqueDateMapList)):
+        heroStats = pd.DataFrame(columns=['hero', 'elims', 'damage', 'healing','deaths'])
+        #for each unique hero played
+        for j in range(len(uniqueHeroesList)):
           #For each row of data
           for row in self.allPlayerData.itertuples(index=False):
               #if row of data has stat we want(elims/damage/healing from statName), store stat in elims/damage/healing
               if uniqueDateMapList[i][0] == row.start_time and uniqueDateMapList[i][1] == row.map_name:
-                  if statName[0] == row.stat_name and 'All Heroes' == row.hero_name: #elims
+                  if statName[0] == row.stat_name and uniqueHeroesList[j] == row.hero_name: #elims
                       elims   = float(row.stat_amount)
-                  elif statName[1] == row.stat_name and 'All Heroes' == row.hero_name: #damage
+                  elif statName[1] == row.stat_name and uniqueHeroesList[j] == row.hero_name: #damage
                       damage  = float(row.stat_amount)
-                  elif statName[2] == row.stat_name and 'All Heroes' == row.hero_name: #healing
+                  elif statName[2] == row.stat_name and uniqueHeroesList[j] == row.hero_name: #healing
                       healing = float(row.stat_amount)
-                  elif statName[3] == row.stat_name and 'All Heroes' == row.hero_name: #deaths
+                  elif statName[3] == row.stat_name and uniqueHeroesList[j] == row.hero_name: #deaths
                       deaths = float(row.stat_amount)                            
           #If elims/damage/healing was stored (not None), append to list
-          if elims != None:
-              elimsList.append(elims)
-          else:
-              elimsList.append(0)                
-          if damage != None:
-              damageList.append(damage)
-          else:
-              damageList.append(0)                
-          if healing != None:
-              healingList.append(healing)
-          else:
-              healingList.append(0)
-          if deaths != None:
-              deathsList.append(deaths)
-          else:
-              deathsList.append(0)
-          elims,damage,healing,deaths = None,None,None,None       
-      
+          if elims == None:
+              elims = 0            
+          if damage == None:
+              damage = 0             
+          if healing == None:
+              healing = 0
+          if deaths == None:
+              deaths = 0
+          heroStats.loc[len(heroStats)] = [uniqueHeroesList[j],elims,damage,healing,deaths]
+          elims,damage,healing,deaths = None,None,None,None
+        perMapPerHeroDFs.append(heroStats)
+
+      for df in perMapPerHeroDFs:
+        elimsList.append(df['elims'].sum())
+        damageList.append(df['damage'].sum())
+        healingList.append(df['healing'].sum())
+        deathsList.append(df['deaths'].sum())
       #Sum elim,damage, and healing scores and sort in descending order
       #zip allows you to associate elements in multiple lists based on their index.  So the result would be
       #[(elimList[1], damageList[1],healingList[1]),(elimList[2]...)]
